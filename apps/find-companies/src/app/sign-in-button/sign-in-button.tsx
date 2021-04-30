98a3533cf3cb7c38ee3e28b2e8bcc21595ca6c11
Thom Veldpaus <thom.veldpaus.werkadres@gmail.com>
2021-04-30 05:27:23 +0200

    created a sign in component

---
+++ apps/find-companies/src/app/sign-in-button/sign-in-button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './sign-in-button.module.scss';
+
+/* eslint-disable-next-line */
+export interface SignInButtonProps {}
+
+export function SignInButton(props: SignInButtonProps) {
+  return (
+    <div>
+      <h1>Welcome to sign-in-button!</h1>
+    </div>
+  );
+}
+
+export default SignInButton;
